3ae753a1e2804889b75b4d0d409e0bd246b7988b
Seonwoo <nsw6662@gmail.com>
2018-06-21 21:20:01 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+
+    [SerializeField]
+    private float thrusterForce = 1000f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        //움직임 계산
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * zMov;
+
+        Vector3 velocity = (movHorizontal + movVertical).normalized * speed;
+
+        //움직임 적용
+        motor.Move(velocity);
+
+        //rotation 계산(에임)
+        float yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 rotation = new Vector3(0f, yRot, 0f) * lookSensitivity;
+
+        //rotation 적용(에임)
+        motor.Rotate(rotation);
+
+        //카메라 회전 계산
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 cameraRotation = new Vector3(xRot, 0f, 0f) * lookSensitivity;
+
+        //카메라 회전 적용
+        motor.RotateCamera(cameraRotation);
+
+        //움직임(thrusterForce) 계산
+        Vector3 _thrusterForce = Vector3.zero;
+
+        if (Input.GetButton("Jump"))
+        {
+            _thrusterForce = Vector3.up * thrusterForce;
+        }
+
+        //움직임(thrusterForce) 적용
+        motor.ApplyThruster(_thrusterForce);
+    }
+}
